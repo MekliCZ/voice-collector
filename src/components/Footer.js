119c2d1b5898bae10272cdb83642a35ceab485b0
Michal Vašíček <me@michalvasicek.cz>
2018-02-24 21:20:49 +0100

    Remove social icons from footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import './Footer.css';
 import FontAwesomeIcon from '@fortawesome/react-fontawesome';
-import {faFacebook, faGithub} from '@fortawesome/fontawesome-free-brands';
 
 export default class Footer extends Component {
     render() {
@@ -9,10 +8,6 @@ export default class Footer extends Component {
             <footer>
                 <div className="mainIcon">
                     <a href="https://www.mozilla.cz"><img className="icon" src="/logo.svg" /></a>
-                </div>
-                <div className="icons">
-                    <a href="https://github.com/MozillaCZ/"><FontAwesomeIcon className="icon" icon={faGithub}/></a>
-                    <a href="https://github.com/MozillaCZ/"><FontAwesomeIcon className="icon" icon={faFacebook}/></a>
                 </div>
             </footer>
         );
